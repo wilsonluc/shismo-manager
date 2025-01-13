7db5ffc3e8a78eda41fc4494a7d4cdf9978cdf10
Wilson <wilsonluc09@gmail.com>
2025-01-13 22:03:29 +1100

    Select skill & select plugin buttons

--- src/components/queue/queueEditorContainer.tsx
+++ src/components/queue/queueEditorContainer.tsx
@@ -6,7 +6,6 @@ import { getSkillBySkillName, skills } from "./skill";
 import { plugins } from "./plugin";
 import Image from "next/image";
 
-// Define a form component inside the QueueEditorContainer
 const QueueEditorContainer = ({
   setTasks,
 }: {
@@ -15,7 +14,8 @@ const QueueEditorContainer = ({
   const [getSkillName, setSkillName] = useState("");
   const [getLevel, setLevel] = useState("");
   const [getPlugin, setPlugin] = useState("");
-  const [showSkillIcons, setShowSkillIcons] = useState(false); // State to toggle skill icons visibility
+  const [showSkillIcons, setShowSkillIcons] = useState(false);
+  const [showPlugins, setShowPlugins] = useState(false);
 
   const handleAddCard = () => {
     // If fields are empty
@@ -42,28 +42,45 @@ const QueueEditorContainer = ({
       pluginName: getPlugin,
     };
 
-    // Add the new card to the queue
     setTasks((prevCards) => [...prevCards, newCard]);
-    setSkillName(""); // Clear input field after adding
-    setLevel(""); // Clear input field after adding
-    setPlugin(""); // Clear input field after adding
+    setSkillName("");
+    setLevel("");
+    setPlugin("");
   };
 
   return (
     <Dropdown
       title="Queue Editor"
-      icon={<ChevronIcon isOpen={false} />} // Default not open
+      icon={<ChevronIcon isOpen={false} />}
       content={
         <div>
-          <div className="flex">
+          <div className="flex gap-1">
             {/* Skill Selector Button */}
             <button
-              onClick={() => setShowSkillIcons((prev) => !prev)} // Toggle the visibility of icons
+              onClick={() => {
+                setShowSkillIcons((prev) => !prev);
+                setShowPlugins(false);
+              }} // Toggle the visibility of icons
               className={`w-full p-2 mb-4 border border-neutral-500 rounded h-10 ${
-                showSkillIcons ? "bg-blue-500 text-white" : "bg-white text-black"
+                showSkillIcons
+                  ? "bg-blue-500 text-white"
+                  : "bg-black text-white"
               }`} // Change button color based on skill selection
             >
               Select Skill
+            </button>
+
+            {/* Plugin Input */}
+            <button
+              onClick={() => {
+                setShowSkillIcons(false);
+                setShowPlugins((prev) => !prev);
+              }} // Toggle the visibility of icons
+              className={`w-full p-2 mb-4 border border-neutral-500 rounded h-10 ${
+                showPlugins ? "bg-blue-500 text-white" : "bg-black text-white"
+              }`} // Change button color based on skill selection
+            >
+              Select Plugin
             </button>
 
             {/* Level Input */}
@@ -74,44 +91,47 @@ const QueueEditorContainer = ({
               placeholder="Level"
               className="w-full p-2 mb-4 border border-neutral-500 rounded text-black h-10"
             />
-
-            {/* Plugin Input */}
-            <select
-              value={getPlugin}
-              onChange={(e) => setPlugin(e.target.value)}
-              className="w-full p-2 mb-4 border border-neutral-500 rounded text-black h-10"
-            >
-              <option value="">Plugin</option>
-              {plugins.map((plugin, index) => (
-                <option key={index} value={plugin}>
-                  {plugin}
-                </option>
-              ))}
-            </select>
           </div>
 
-          {/* This is where the skill icons will go */}
           <div className="mb-4">
+            {/* Select Skill */}
             {showSkillIcons && (
               <div className="skill-icons-container grid grid-cols-3 gap-2">
                 {skills.map((skill) => (
                   <div
                     key={skill.skillName}
-                    onClick={() => setSkillName(skill.skillName)} // Set skill when clicked
+                    onClick={() => setSkillName(skill.skillName)}
                     className={`skill-icon cursor-pointer p-2 text-center border border-neutral-300 rounded ${
                       getSkillName === skill.skillName
-                        ? "bg-blue-500 text-white" // Highlight selected skill
+                        ? "bg-blue-500 text-white"
                         : ""
                     }`}
                   >
                     <Image
-                      src={skill.iconPath} // Assuming each skill has an `iconPath` field
+                      src={skill.iconPath}
                       alt={skill.skillName}
-                      width={32} // The width of the image (adjust as needed)
-                      height={32} // The height of the image (adjust as needed)
-                      className="mx-auto" // Centering the image horizontally
+                      width={32}
+                      height={32}
+                      className="mx-auto"
                     />
-                    <p>{skill.skillName}</p> {/* Display skill name under the icon */}
+                    <p>{skill.skillName}</p>{" "}
+                  </div>
+                ))}
+              </div>
+            )}
+
+            {/* Select Plugin */}
+            {showPlugins && (
+              <div className="skill-icons-container grid grid-cols-2 gap-2">
+                {plugins.map((plugin) => (
+                  <div
+                    key={plugin}
+                    onClick={() => setPlugin(plugin)}
+                    className={`skill-icon cursor-pointer p-2 text-center border border-neutral-300 rounded ${
+                      getPlugin === plugin ? "bg-blue-500 text-white" : ""
+                    }`}
+                  >
+                    <p>{plugin}</p>
                   </div>
                 ))}
               </div>
