3ccbc763fb43e9471d12704fb42366f7e79e109a
Wilson <wilsonluc09@gmail.com>
2025-01-20 01:49:46 +1100

    Websocket sync integration

--- server.js
+++ server.js
@@ -1,54 +1,63 @@
-import express from 'express';
-import cors from 'cors';
-import next from 'next';
-import passport from 'passport';
-import passportDiscord from 'passport-discord';
-import { config } from 'dotenv';
-import session from 'express-session';
+import express from "express";
+import cors from "cors";
+import next from "next";
+import passport from "passport";
+import passportDiscord from "passport-discord";
+import { config } from "dotenv";
+import session from "express-session";
 
 config();
 
 const { Strategy: DiscordStrategy } = passportDiscord;
-const app = next({ dev: process.env.NODE_ENV !== 'production' });
+const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
 
   // Enable CORS
-  server.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'PUT', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+  server.use(
+    cors({
+      origin: "http://localhost:3000",
+      methods: ["GET", "PUT", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
 
   // Initialize session middleware
-  server.use(session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: false
-  }));
+  server.use(
+    session({
+      secret: "secret",
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
 
   // Initialize passport after session middleware
   server.use(passport.initialize());
   server.use(passport.session());
 
   // Passport Discord strategy setup
-  passport.use(new DiscordStrategy({
-    clientID: process.env.DISCORD_CLIENT_ID,
-    clientSecret: process.env.DISCORD_CLIENT_SECRET,
-    callbackURL: process.env.DISCORD_CALLBACK_URL,
-    scope: ['identify', 'email'],
-  }, (accessToken, refreshToken, profile, done) => {
-    return done(null, profile);
-  }));
+  passport.use(
+    new DiscordStrategy(
+      {
+        clientID: process.env.DISCORD_CLIENT_ID,
+        clientSecret: process.env.DISCORD_CLIENT_SECRET,
+        callbackURL: process.env.DISCORD_CALLBACK_URL,
+        scope: ["identify", "email"],
+      },
+      (accessToken, refreshToken, profile, done) => {
+        return done(null, profile);
+      }
+    )
+  );
 
   // Serialize and deserialize user
   passport.serializeUser((user, done) => done(null, user));
   passport.deserializeUser((user, done) => done(null, user));
 
   // Route to fetch user data
-  server.get('/api/user', (req, res) => {
+  server.get("/api/user", (req, res) => {
     if (req.isAuthenticated()) {
       res.status(200).json({ user: req.user });
     } else {
@@ -57,15 +66,19 @@ app.prepare().then(() => {
   });
 
   // Discord OAuth routes
-  server.get('/auth/discord', passport.authenticate('discord'));
-  server.get('/auth/discord/callback', passport.authenticate('discord', {
-    failureRedirect: '/',
-  }), (req, res) => {
-    res.redirect('/');
-  });
+  server.get("/auth/discord", passport.authenticate("discord"));
+  server.get(
+    "/auth/discord/callback",
+    passport.authenticate("discord", {
+      failureRedirect: "/",
+    }),
+    (req, res) => {
+      res.redirect("/");
+    }
+  );
 
   // Logout
-  server.post('/api/logout', (req, res) => {
+  server.post("/api/logout", (req, res) => {
     req.logout((err) => {
       if (err) {
         return res.status(500).json({ message: "Failed to log out" });
@@ -81,13 +94,13 @@ app.prepare().then(() => {
   });
 
   // Catch-all route handler for Next.js pages
-  server.all('*', (req, res) => {
+  server.all("*", (req, res) => {
     return handle(req, res);
   });
 
   // Start the server
   server.listen(process.env.PORT, (err) => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
+    console.log("> Ready on http://localhost:3000");
   });
 });
--- src/components/sync/sync.tsx
+++ src/components/sync/sync.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useRef } from "react";
 import { TASKS_ENDPOINT } from "../../app/endpoints";
 import { Task } from "../../app/page";
 import { parseTasksJsonToString } from "../queue/queue";
@@ -10,40 +11,85 @@ interface SyncProps {
 }
 
 const Sync: React.FC<SyncProps> = ({ tasks, characterName }) => {
+  // Websocket reference
+  const socketRef = useRef<WebSocket | null>(null);
+
   // Function to send the PUT request to update tasks
-  const setTasks = async () => {
+  // const setTasks = async () => {
+  //   try {
+  //     const response = await fetch("/api/user");
+  //     const data = await response.json();
+
+  //     if (data.user && characterName) {
+  //       const tasksResponse = await fetch(
+  //         TASKS_ENDPOINT + data.user.id + "/" + characterName,
+  //         {
+  //           method: "PUT",
+  //           headers: {
+  //             "Content-Type": "application/json",
+  //           },
+  //           body: JSON.stringify({ tasks: parseTasksJsonToString(tasks) }),
+  //         }
+  //       );
+
+  //       const tasksJson = await tasksResponse.json();
+  //       if (tasksResponse.ok) {
+  //         console.log("Tasks updated successfully:", tasksJson);
+  //       } else {
+  //         console.error("Failed to update tasks:", tasksJson);
+  //       }
+  //     }
+  //   } catch (error) {
+  //     console.error("Error fetching user/characterName data:", error);
+  //   }
+  // };
+
+  const syncWebSocket = async () => {
     try {
       const response = await fetch("/api/user");
       const data = await response.json();
 
       if (data.user && characterName) {
-        const tasksResponse = await fetch(
-          TASKS_ENDPOINT + data.user.id + "/" + characterName,
-          {
-            method: "PUT",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ tasks: parseTasksJsonToString(tasks) }),
-          }
-        );
+        const wsUrl = `wss://cn2xa6jdhj.execute-api.eu-west-2.amazonaws.com/production/?discordID=${data.user.id}&characterName=${characterName}`;
+        socketRef.current = new WebSocket(wsUrl);
 
-        const tasksJson = await tasksResponse.json();
-        if (tasksResponse.ok) {
-          console.log("Tasks updated successfully:", tasksJson);
-        } else {
-          console.error("Failed to update tasks:", tasksJson);
-        }
+        socketRef.current.onopen = () => {
+          console.log("WebSocket connected");
+
+          const params = {
+            action: "sync", // route
+            discordID: data.user.id,
+            characterName: characterName,
+            isOnline: false,
+            skills: null,
+            tasks: parseTasksJsonToString(tasks),
+            currentTaskID: null,
+          };
+
+          socketRef.current.send(JSON.stringify(params));
+        };
+
+        socketRef.current.onmessage = (event) => {
+          console.log("Received message: " + event.data);
+        };
+
+        socketRef.current.onerror = (error) => {
+          console.error("WebSocket error:", error);
+        };
+        
+        socketRef.current.onclose = () => {
+          console.log("WebSocket connection closed");
+        };
       }
     } catch (error) {
-      console.error("Error fetching user/characterName data:", error);
+      console.error("Error setting up websocket", error);
     }
   };
 
   return (
     <div className="absolute top-0 right-12">
       <button
-        onClick={setTasks}
+        onClick={syncWebSocket}
         className="relative inline-flex h-10 w-10 items-center justify-center rounded-lg    border border-border bg-background/50 hover:bg-accent    transition-colors duration-200"
         title="Sync with cloud"
       >
